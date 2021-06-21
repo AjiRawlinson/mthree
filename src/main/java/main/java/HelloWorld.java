65aa90094ea5467c60a8742368a54a66e6a8299f
Aji <aji.rawlinson@gmail.com>
2021-06-21 14:01:36 +0100

    0001 First Commit

---
+++ src/main/java/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Aji");
+        System.out.println("Hello Dublin");
+        System.out.println("Hello 0852690325");
+        System.out.println("Hello mthree class c103");
+    }
+}
